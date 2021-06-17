4c4abd484ec0d6c4f26de661d061eb5698779f45
egomezsalasva <egomezsalasva@gmail.com>
2021-06-17 17:03:11 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+
+module.exports = withPlugins([withImages], {
+  target: 'serverless',
+  reactStrictMode: true
+})
