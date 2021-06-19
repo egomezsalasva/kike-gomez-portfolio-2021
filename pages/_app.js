5cd0debb11504175af551e56e912e21624ee4109
egomezsalasva <egomezsalasva@gmail.com>
2021-06-19 15:57:25 +0200

    styled components setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import { createGlobalStyle } from 'styled-components'
+import { brandingColors } from '../styles/customStyles'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: ${brandingColors.light};
+  }
+`
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
